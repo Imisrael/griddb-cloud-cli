d66695dfe84f41fb7db011b7ff975de81446e26b
Israel Imru <imru@fixstars.com>
2025-04-17 11:40:18 -0700

    preliminary printing out all float/double/int values onto graph

--- cmd/readContainer/readIntoGraph.go
+++ cmd/readContainer/readIntoGraph.go
@@ -9,26 +9,20 @@ import (
 	"net/http"
 	"strconv"
 
+	"github.com/guptarohit/asciigraph"
 	"github.com/spf13/cobra"
 	"griddb.net/griddb-cloud-cli/cmd"
 )
 
-var (
-	offset int
-	limit  int
-	pretty bool
-	raw    bool
-)
-
 func init() {
-	cmd.RootCmd.AddCommand(readContainerCmd)
-	readContainerCmd.Flags().IntVar(&offset, "offset", 0, "How many rows you'd like to offset in your query")
-	readContainerCmd.Flags().IntVar(&limit, "limit", 100, "How many rows you'd like to limit")
-	readContainerCmd.Flags().BoolVarP(&pretty, "pretty", "p", false, "Print the JSON with Indent rules")
-	readContainerCmd.Flags().BoolVar(&raw, "raw", false, "When enabled, will simply output direct results from GridDB Cloud")
+	readContainerCmd.AddCommand(readIntoGraph)
+	readIntoGraph.Flags().IntVar(&offset, "offset", 0, "How many rows you'd like to offset in your query")
+	readIntoGraph.Flags().IntVar(&limit, "limit", 100, "How many rows you'd like to limit")
+	readIntoGraph.Flags().BoolVarP(&pretty, "pretty", "p", false, "Print the JSON with Indent rules")
+	readIntoGraph.Flags().BoolVar(&raw, "raw", false, "When enabled, will simply output direct results from GridDB Cloud")
 }
 
-func readContainer(containerName string) {
+func getGraphData(containerName string) [][]cmd.QueryData {
 	client := &http.Client{}
 	convert := []byte(
 		"{   \"offset\" : " + strconv.Itoa(offset) + ",   \"limit\": " + strconv.Itoa(limit) + "}",
@@ -51,21 +45,11 @@ func readContainer(containerName string) {
 	if err != nil {
 		fmt.Println("Error with reading body! ", err)
 	}
-	if raw {
-		fmt.Println(string(body))
-	} else {
-		parseBody(body, pretty)
-	}
-
-}
-
-func parseBody(body []byte, pretty bool) {
 	var results cmd.CloudResults
 
 	if err := json.Unmarshal(body, &results); err != nil {
 		panic(err)
 	}
-	//fmt.Println(results)
 	var cols []cmd.Columns = results.Columns
 	var rows [][]any = results.Rows
 	var rowsLength int
@@ -85,31 +69,39 @@ func parseBody(body []byte, pretty bool) {
 		}
 	}
 
-	if pretty {
-		jso, err := json.MarshalIndent(data, "", "    ")
-		if err != nil {
-			fmt.Println("Error", err)
-		}
-		fmt.Println(string(jso))
-	} else {
-		jso, err := json.Marshal(data)
-		if err != nil {
-			fmt.Println("Error", err)
-		}
-		fmt.Println(string(jso))
-	}
-
+	return data
 }
 
-var readContainerCmd = &cobra.Command{
-	Use:   "read",
+func graphIt(data [][]cmd.QueryData) {
+
+	var rows [][]float64 = make([][]float64, len(data))
+
+	for i := range data {
+		rows[i] = make([]float64, 1)
+		for j := range data[i] {
+			if data[i][j].Type == "FLOAT" || data[i][j].Type == "INTEGER" || data[i][j].Type == "DOUBLE" {
+				fmt.Println(i, j)
+				fmt.Println(data[i][j])
+				rows[i] = append(rows[i], data[i][j].Value.(float64))
+			}
+		}
+	}
+	fmt.Println(rows)
+
+	graph := asciigraph.PlotMany(rows, asciigraph.Height(20))
+	fmt.Println(graph)
+}
+
+var readIntoGraph = &cobra.Command{
+	Use:   "graph",
 	Short: "Read container",
 	Long:  "Read container and print out table",
 	Run: func(cmd *cobra.Command, args []string) {
 		if len(args) > 1 {
 			log.Fatal("you may only read from one container at a time")
 		} else if len(args) == 1 {
-			readContainer(args[0])
+			data := getGraphData(args[0])
+			graphIt(data)
 		} else {
 			log.Fatal("Please include the container name you'd like to read from!")
 		}
