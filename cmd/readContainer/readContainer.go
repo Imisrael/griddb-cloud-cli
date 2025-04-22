27f8f4cdde0590510c738005e2c78c11adf6fdd7
Israel Imru <imru@fixstars.com>
2025-04-22 09:43:10 -0700

    added tql reading for non graph

--- cmd/readContainer/readContainer.go
+++ cmd/readContainer/readContainer.go
@@ -8,16 +8,18 @@ import (
 	"log"
 	"net/http"
 	"strconv"
+	"strings"
 
 	"github.com/spf13/cobra"
 	"griddb.net/griddb-cloud-cli/cmd"
 )
 
 var (
-	offset int
-	limit  int
-	pretty bool
-	raw    bool
+	offset     int
+	limit      int
+	pretty     bool
+	raw        bool
+	colToGraph string
 )
 
 func init() {
@@ -26,6 +28,34 @@ func init() {
 	readContainerCmd.Flags().IntVar(&limit, "limit", 100, "How many rows you'd like to limit")
 	readContainerCmd.Flags().BoolVarP(&pretty, "pretty", "p", false, "Print the JSON with Indent rules")
 	readContainerCmd.Flags().BoolVar(&raw, "raw", false, "When enabled, will simply output direct results from GridDB Cloud")
+	readContainerCmd.Flags().StringVar(&colToGraph, "colNames", "", "Which columns would you like to see charted (separated by commas!)")
+}
+
+func wrapInTqlObj(containerName string) string {
+
+	//EXAMPLE [{"name" : "device1", "stmt" : "select * limit 100", "columns" : ["co", "humidity"], "hasPartialExecution" : true}]
+	cols := unfurlUserColChoice()
+	s := "[ { \"name\": \"" + containerName + "\", \"stmt\": \"select * limit " + strconv.Itoa(limit) + "\", \"columns\": " + cols + ", \"hasPartialExecution\": true }]"
+	fmt.Println(s)
+	return s
+}
+
+func unfurlUserColChoice() string {
+	if len(colToGraph) > 1 {
+		removeSpace := strings.ReplaceAll(colToGraph, " ", "")
+		turnToSlice := strings.Split(removeSpace, ",")
+		s := "["
+		for _, val := range turnToSlice {
+			dblQuote := "\"" + val + "\","
+			s = s + dblQuote
+		}
+		s = strings.Trim(s, ",")
+		s = s + "]"
+		//	fmt.Println(s)
+		return s
+	} else {
+		return "null"
+	}
 }
 
 func readContainer(containerName string) {
@@ -59,15 +89,79 @@ func readContainer(containerName string) {
 
 }
 
+func readTql(containerName string) [][]cmd.QueryData {
+	client := &http.Client{}
+
+	stmt := wrapInTqlObj(containerName)
+	stmtBytes := []byte(stmt)
+	buf := bytes.NewBuffer(stmtBytes)
+
+	req, err := cmd.MakeNewRequest("POST", "/tql/", buf)
+	if err != nil {
+		fmt.Println("Error making new request", err)
+	}
+
+	resp, err := client.Do(req)
+	if err != nil {
+		fmt.Println("error with client DO: ", err)
+	}
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Println("Error with reading body! ", err)
+	}
+
+	if resp.StatusCode == 400 {
+		var errorMsg cmd.ErrorMsg
+		if err := json.Unmarshal(body, &errorMsg); err != nil {
+			panic(err)
+		}
+		log.Fatal("Reading Container ERROR: " + errorMsg.ErrorMessage)
+	}
+
+	var results []cmd.TQLResults
+
+	if err := json.Unmarshal(body, &results); err != nil {
+		panic(err)
+	}
+	var cols []cmd.Columns = results[0].Columns
+	var rows [][]any = results[0].Results
+	var rowsLength int
+
+	if len(rows) > 0 {
+		rowsLength = len(rows)
+	}
+
+	var data [][]cmd.QueryData = make([][]cmd.QueryData, rowsLength)
+
+	for i := range rows {
+		data[i] = make([]cmd.QueryData, len(rows[i]))
+		for j := range rows[i] {
+			data[i][j].Name = cols[j].Name
+			data[i][j].Type = cols[j].Type
+			data[i][j].Value = rows[i][j]
+		}
+	}
+
+	if raw {
+		fmt.Println(string(body))
+	} else {
+		parseBody(body, pretty)
+	}
+
+	return data
+
+}
+
 func parseBody(body []byte, pretty bool) {
-	var results cmd.CloudResults
+	var results []cmd.TQLResults
 
 	if err := json.Unmarshal(body, &results); err != nil {
 		panic(err)
 	}
 	//fmt.Println(results)
-	var cols []cmd.Columns = results.Columns
-	var rows [][]any = results.Rows
+	var cols []cmd.Columns = results[0].Columns
+	var rows [][]any = results[0].Results
 	var rowsLength int
 
 	if len(rows) > 0 {
@@ -109,7 +203,7 @@ var readContainerCmd = &cobra.Command{
 		if len(args) > 1 {
 			log.Fatal("you may only read from one container at a time")
 		} else if len(args) == 1 {
-			readContainer(args[0])
+			readTql(args[0])
 		} else {
 			log.Fatal("Please include the container name you'd like to read from!")
 		}
--- cmd/readContainer/readIntoGraph.go
+++ cmd/readContainer/readIntoGraph.go
@@ -1,22 +1,12 @@
 package readContainer
 
 import (
-	"bytes"
-	"encoding/json"
 	"fmt"
-	"io"
 	"log"
-	"net/http"
-	"strconv"
-	"strings"
 
 	"github.com/guptarohit/asciigraph"
 	"github.com/spf13/cobra"
 	"griddb.net/griddb-cloud-cli/cmd"
-)
-
-var (
-	colToGraph string
 )
 
 func init() {
@@ -26,99 +16,17 @@ func init() {
 	readIntoGraph.Flags().StringVar(&colToGraph, "colNames", "", "Which columns would you like to see charted (separated by commas!)")
 }
 
-func unfurlUserColChoice() string {
-	if len(colToGraph) > 1 {
-		removeSpace := strings.ReplaceAll(colToGraph, " ", "")
-		turnToSlice := strings.Split(removeSpace, ",")
-		s := "["
-		for _, val := range turnToSlice {
-			dblQuote := "\"" + val + "\","
-			s = s + dblQuote
-		}
-		s = strings.Trim(s, ",")
-		s = s + "]"
-		//	fmt.Println(s)
-		return s
-	} else {
-		return "null"
-	}
-}
-
-func wrapInTqlObj(containerName string) string {
-
-	//EXAMPLE [{"name" : "device1", "stmt" : "select * limit 100", "columns" : ["co", "humidity"], "hasPartialExecution" : true}]
-	cols := unfurlUserColChoice()
-	s := "[ { \"name\": \"" + containerName + "\", \"stmt\": \"select * limit " + strconv.Itoa(limit) + "\", \"columns\": " + cols + ", \"hasPartialExecution\": true }]"
-	fmt.Println(s)
-	return s
-}
-
-func readTql(containerName string) [][]cmd.QueryData {
-	client := &http.Client{}
-
-	stmt := wrapInTqlObj(containerName)
-	stmtBytes := []byte(stmt)
-	buf := bytes.NewBuffer(stmtBytes)
-
-	req, err := cmd.MakeNewRequest("POST", "/tql/", buf)
-	if err != nil {
-		fmt.Println("Error making new request", err)
-	}
-
-	resp, err := client.Do(req)
-	if err != nil {
-		fmt.Println("error with client DO: ", err)
-	}
-
-	body, err := io.ReadAll(resp.Body)
-	if err != nil {
-		fmt.Println("Error with reading body! ", err)
-	}
-
-	if resp.StatusCode == 400 {
-		var errorMsg cmd.ErrorMsg
-		if err := json.Unmarshal(body, &errorMsg); err != nil {
-			panic(err)
-		}
-		log.Fatal("Reading Container ERROR: " + errorMsg.ErrorMessage)
-	}
-
-	var results []cmd.TQLResults
-
-	if err := json.Unmarshal(body, &results); err != nil {
-		panic(err)
-	}
-	var cols []cmd.Columns = results[0].Columns
-	var rows [][]any = results[0].Results
-	var rowsLength int
-
-	if len(rows) > 0 {
-		rowsLength = len(rows)
-	}
-
-	var data [][]cmd.QueryData = make([][]cmd.QueryData, rowsLength)
-
-	for i := range rows {
-		data[i] = make([]cmd.QueryData, len(rows[i]))
-		for j := range rows[i] {
-			data[i][j].Name = cols[j].Name
-			data[i][j].Type = cols[j].Type
-			data[i][j].Value = rows[i][j]
-		}
-	}
-
-	return data
-
-}
-
-func graphIt(data [][]cmd.QueryData) {
+func graphIt(data [][]cmd.QueryData, containerName string) {
 
 	var m map[string][]float64 = make(map[string][]float64)
+	var rejects map[string]string = make(map[string]string)
 
 	for i := range data {
 		for j := range data[i] {
 			if data[i][j].Type == "FLOAT" || data[i][j].Type == "INTEGER" || data[i][j].Type == "DOUBLE" {
 				m[data[i][j].Name] = append(m[data[i][j].Name], data[i][j].Value.(float64))
+			} else {
+				rejects[data[i][j].Name] = data[i][j].Type
 			}
 		}
 	}
@@ -133,13 +41,16 @@ func graphIt(data [][]cmd.QueryData) {
 		colNames[i] = rowName
 		i++
 	}
+	for rejectName, rejectType := range rejects {
+		fmt.Println("Column " + rejectName + " (of type " + rejectType + " ) is not a `number` type. Omitting")
+	}
 
 	graph := asciigraph.PlotMany(
 		rows,
-		asciigraph.Height(30),
+		asciigraph.Height(0),
 		asciigraph.SeriesColors(asciigraph.Red, asciigraph.Green, asciigraph.Blue, asciigraph.Pink, asciigraph.Orange),
 		asciigraph.SeriesLegends(colNames...),
-		asciigraph.Caption("Series with legends"),
+		asciigraph.Caption("Col names from container "+containerName),
 		asciigraph.Width(100),
 	)
 	fmt.Println(graph)
@@ -154,7 +65,7 @@ var readIntoGraph = &cobra.Command{
 			log.Fatal("you may only read from one container at a time")
 		} else if len(args) == 1 {
 			data := readTql(args[0])
-			graphIt(data)
+			graphIt(data, args[0])
 		} else {
 			log.Fatal("Please include the container name you'd like to read from!")
 		}
